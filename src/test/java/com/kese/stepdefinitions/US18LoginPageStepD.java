361c95bbb61f8f750e494cd47ae85b488c32541f
bilalsr <bilalsari13@gmail.com>
2022-01-05 20:18:50 -0500

    added my US

---
+++ src/test/java/com/kese/stepdefinitions/US18LoginPageStepD.java
@@ -0,0 +1,81 @@
+package com.kese.stepdefinitions;
+
+import com.kese.pages.US18LoginPage;
+import com.kese.utilities.BrowserUtils;
+import com.kese.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+
+public class US18LoginPageStepD {
+
+    //US18LoginPage us18LoginPage=new US18LoginPage();
+
+    @Given("user is on login page {string}")
+    public void userIsOnLoginPage(String loginPageUrl) {
+
+        Driver.get().get(loginPageUrl);
+    }
+
+    @When("user enters valid username {string}")
+    public void userEntersValidUsername(String userName) {
+
+        US18LoginPage.enterUserName(userName);
+    }
+
+    @And("user enters valid password {string}")
+    public void userEntersValidPassword(String password) {
+
+        US18LoginPage.enterPassword(password);
+    }
+
+    @And("user clicks on Login button")
+    public void userClicksOnLoginButton() {
+
+        US18LoginPage.clickSubmitbutton();
+    }
+
+    @Then("user gets the url of the page")
+    public void userGetsTheUrlOfThePage() {
+
+        String currentUrl= BrowserUtils.myGetCurrentUrlMethod();
+
+    }
+
+    @And("page url should be {string}")
+    public void pageUrlShouldBe(String expectedUrl) {
+
+
+        Assert.assertTrue((Driver.get().getCurrentUrl().contains(expectedUrl)));
+    }
+
+    @And("user enters invalid password {string}")
+    public void userEntersInvalidPassword(String invalidPassword) {
+
+        US18LoginPage.enterPassword(invalidPassword);
+
+    }
+
+    @Then("user should get the this message {string}")
+    public void userShouldGetTheThisMessage(String expectederrorMessage) {
+
+        String currentErrorMessage =US18LoginPage.getErrorMessage();
+        Assert.assertTrue(expectederrorMessage.contains(currentErrorMessage));
+
+    }
+
+    @When("users enter invalid username {string}")
+    public void usersEnterInvalidUsername(String invalidUsername) {
+
+        US18LoginPage.enterUserName(invalidUsername);
+    }
+
+    @When("user enters invalid username {string}")
+    public void userEntersInvalidUsername(String invalidUsername) {
+
+        US18LoginPage.enterUserName(invalidUsername);
+    }
+}
---
+++ src/test/resources/features/US18KeseLoginPage.feature
@@ -0,0 +1,32 @@
+Feature: Kese Login page feature
+
+  @main
+  Scenario: Kese Website Login with valid username valid password
+    Given user is on login page "http://test.kese.nl/login"
+    When user enters valid username "email@gmail.com"
+    And user enters valid password "3344"
+    And user clicks on Login button
+    Then user gets the url of the page
+    And page url should be "http://test.kese.nl/"
+
+  Scenario: Kese Website Login with valid username invalid password
+    Given user is on login page "http://test.kese.nl/login"
+    When user enters valid username "email@gmail.com"
+    And user enters invalid password "abcd"
+    And user clicks on Login button
+    Then user should get the this message "Email ya da şifre hatalı."
+
+
+  Scenario: Kese Website Login with invalid username valid password
+    Given user is on login page "http://test.kese.nl/login"
+    When users enter invalid username "zmail@yahoo.com"
+    And user enters valid password "3344"
+    And user clicks on Login button
+    Then user should get the this message "Email ya da şifre hatalı."
+
+  Scenario: Kese Website Login with invalid username invalid password
+    Given user is on login page "http://test.kese.nl/login"
+    When user enters invalid username "xmail@gmail.com"
+    And user enters invalid password "ol56"
+    And user clicks on Login button
+    Then user should get the this message "Email ya da şifre hatalı."
