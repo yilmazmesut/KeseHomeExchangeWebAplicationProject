56aba5f6784240a81ad0ff6cedb2a0089b1345ff
maykicn <maykic@gmail.com>
2022-01-09 21:16:06 +0100

    Name of SingUpPage is changed to SignUpPage

--- src/test/java/com/kese/pages/SignUpPage.java
+++ src/test/java/com/kese/pages/SignUpPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class SingUpPage extends CommonPage{
+public class SignUpPage extends CommonPage{
 
     @FindBy(xpath = "//a[@href='/usage']")
     public WebElement usage;
