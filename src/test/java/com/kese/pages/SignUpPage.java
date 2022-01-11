d954f7e56990a926f7b251425ec3723ef0c49782
Rej Arbabov <r.arbabov@gmail.com>
2022-01-11 10:05:30 -0700

    Rejep commit January 11

--- src/test/java/com/kese/pages/SignUpPage.java
+++ src/test/java/com/kese/pages/SignUpPage.java
@@ -16,6 +16,25 @@ public class SignUpPage extends CommonPage{
     @FindBy(xpath = "//a[@href='/privacy']")
     public WebElement privacy;
 
+
+    @FindBy(xpath="//input[@type='text']")
+    public WebElement SignupUserInput;
+
+    @FindBy(xpath = "//input[@type='email']")
+    public WebElement SignupEmailBox;
+
+    @FindBy(xpath = "//input[@id='sifre']")
+    public WebElement SignupPasswordBox;
+
+    @FindBy(xpath = "//input[@id='sifreTekrar']")
+    public WebElement SignupPasswordBox2;
+
+    @FindBy(xpath = "//div[@class='d-flex justify-content-center align-items-center']")
+    public WebElement SignupButton;
+
+    @FindBy(xpath = "//input[@type='checkbox']")
+    public WebElement CheckBox;
+
     private static final By girisYapButton = By.linkText("Giriş Yap");
 
 
