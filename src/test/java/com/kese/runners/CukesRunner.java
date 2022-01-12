c04ca8285184ff7a5cf4926bbb33df97eb129ce9
nrslc <cankarnursel@gmail.com>
2022-01-12 12:58:30 +0100

    Nursel- added US009 - Homepage Giris Yap button verification

--- src/test/java/com/kese/runners/CukesRunner.java
+++ src/test/java/com/kese/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
             //    "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/kese/stepdefinitions",
-        tags = "@homepageSlogan",
-        dryRun =false
+        tags = "",
+        dryRun = false
 )
 public class CukesRunner {
 
