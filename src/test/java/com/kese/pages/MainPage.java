4321c9cdc9d09ffc9a11e017e9bfbc790783a2c6
nurveda <utoglunurr@gmail.com>
2022-01-12 13:27:24 +0300

    US07 TC's added also made some changes on cukesRunner

--- src/test/java/com/kese/pages/MainPage.java
+++ src/test/java/com/kese/pages/MainPage.java
@@ -7,4 +7,10 @@ public class MainPage extends CommonPage{
 
     @FindBy(xpath = "//a[@class='navbar-brand']")
     public WebElement logo;
+
+    @FindBy(xpath = "//h2[text()='Popüler İlanlar']")
+    public WebElement headerPopIlan;
+
+    @FindBy(xpath = "//div[@class='tab-pane  active show']//a")
+    public WebElement activeTab;
 }
