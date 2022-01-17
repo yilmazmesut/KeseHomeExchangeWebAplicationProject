d9e59ed58594cd3276a6f47639d0aab0a272d205
Billsara11 <60450435+Billsara11@users.noreply.github.com>
2022-01-17 14:02:03 -0500

    Update LoginPage.java

--- src/test/java/com/kese/pages/LoginPage.java
+++ src/test/java/com/kese/pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage<Webelement> extends CommonPage {
+public class LoginPage extends CommonPage {
 
 
 
