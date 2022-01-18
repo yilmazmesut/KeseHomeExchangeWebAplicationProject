effaebfccf9a0941302a52c655a8381bb5711265
Wo-0-oW <almadm1987@gmail.com>
2022-01-18 20:26:28 +0300

    Adem v1.2

--- src/test/java/com/kese/stepdefinitions/US13SignUpPageCheck.java
+++ src/test/java/com/kese/stepdefinitions/US13SignUpPageCheck.java
@@ -2,7 +2,7 @@ package com.kese.stepdefinitions;
 
 import com.kese.pages.LoginPage;
 import com.kese.pages.MainPage;
-import com.kese.pages.SingUpPage;
+import com.kese.pages.SignUpPage;
 import com.kese.utilities.ConfigurationReader;
 import com.kese.utilities.Driver;
 import io.cucumber.java.bs.A;
@@ -16,7 +16,7 @@ import javax.security.auth.login.Configuration;
 import javax.xml.xpath.XPath;
 
 public class US13SignUpPageCheck {
-    SingUpPage singUpPage = new SingUpPage();
+    SignUpPage signUpPage = new SignUpPage();
     MainPage mainPage = new MainPage();
 
     @Given("Kullanici {string} sayfasina gider")
@@ -27,7 +27,7 @@ public class US13SignUpPageCheck {
 
     @And("Giris Yap butonua tikla")
     public void girisYapButonuaTikla() {
-        mainPage.loginClick.click();
+        mainPage.girisYapButton.click();
     }
 
     @Then("{string} sayfasina gelir")
@@ -37,42 +37,42 @@ public class US13SignUpPageCheck {
 
     @And("Kayit Ol butonuna tikla")
     public void kayitOlButonunaTikla() {
-        singUpPage.registerClick.click();
+        signUpPage.registerClick.click();
     }
 
     @Then("{string} sayfasina gider")
     public void sayfasinaGider(String arg0) {
-        Assert.assertTrue(singUpPage.kayitOlPage.isDisplayed());
+        Assert.assertTrue(signUpPage.kayitOlPage.isDisplayed());
     }
 
 
     @Given("Kullanici {string} yerine {string} yazar")
     public void kullaniciYerineYazar(String arg0, String arg1) {
-       singUpPage.kullaniciAdi.sendKeys(ConfigurationReader.get("kullaniciAdiKayitOlPage"));
-       Assert.assertTrue("Kullanıcı Adı",singUpPage.kullaniciAdi.isDisplayed());
+       signUpPage.kullaniciAdi.sendKeys(ConfigurationReader.get("kullaniciAdiKayitOlPage"));
+       Assert.assertTrue("Kullanıcı Adı",signUpPage.kullaniciAdi.isDisplayed());
     }
 
     @Given("Kullanici email@gmail.com kismina admin@gmail.com girilir")
     public void kullaniciEmailGmailComKisminaAdminGmailComGirilir() {
-        singUpPage.emailAdres.sendKeys(ConfigurationReader.get("emailadresiKayitOlPage"));
-        Assert.assertTrue("email@gmail.com",singUpPage.emailAdres.isDisplayed());
+        signUpPage.emailAdres.sendKeys(ConfigurationReader.get("emailadresiKayitOlPage"));
+        Assert.assertTrue("email@gmail.com",signUpPage.emailAdres.isDisplayed());
     }
 
     @Given("Kullanici Sifre kismina Admin{int}.girilir")
     public void kullaniciSifreKisminaAdminGirilir(int arg0) {
-        singUpPage.sifre.sendKeys(ConfigurationReader.get("sifreKayitOlPage"));
-        Assert.assertTrue("Şifre",singUpPage.sifre.isDisplayed());
+        signUpPage.sifre.sendKeys(ConfigurationReader.get("sifreKayitOlPage"));
+        Assert.assertTrue("Şifre",signUpPage.sifre.isDisplayed());
     }
 
     @Given("Kullanici bir onceki adimda girdigimiz sifre Admin{int}.buraya da girilir")
     public void kullaniciBirOncekiAdimdaGirdigimizSifreAdminBurayaDaGirilir(int arg0) {
-        singUpPage.sifreTekrar.sendKeys(ConfigurationReader.get("sifreTekrarKayitOlPage"));
-        Assert.assertTrue("Şifre tekrarı",singUpPage.sifreTekrar.isDisplayed());
+        signUpPage.sifreTekrar.sendKeys(ConfigurationReader.get("sifreTekrarKayitOlPage"));
+        Assert.assertTrue("Şifre tekrarı",signUpPage.sifreTekrar.isDisplayed());
 
     }
 
     @Given("default email adresinde email@gmail.com yazili oldugunu kontrol edilir")
     public void defaultEmailAdresindeEmailGmailComYaziliOldugunuKontrolEdilir() {
-        Assert.assertTrue("email@gmail.com",singUpPage.emailAdres.isDisplayed());
+        Assert.assertTrue("email@gmail.com",signUpPage.emailAdres.isDisplayed());
     }
 }
