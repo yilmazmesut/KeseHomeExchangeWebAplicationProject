05df0917385ee60a13af91b8e4eaad5d6098b2f8
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2022-01-21 09:37:36 +0100

    US53 Updated with selectable page number

--- src/test/java/com/kese/stepdefinitions/US53HomeExchangePage10StepD.java
+++ src/test/java/com/kese/stepdefinitions/US53HomeExchangePage10StepD.java
@@ -1,10 +1,12 @@
 package com.kese.stepdefinitions;
 
+import com.kese.pages.AAAAASilPage;
 import com.kese.pages.EvDegisimiPage;
 import com.kese.pages.LoginPage;
 import com.kese.utilities.BrowserUtils;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import java.awt.*;
 import java.util.List;
@@ -23,56 +25,79 @@ public class US53HomeExchangePage10StepD {
         BrowserUtils.waitForPageToLoad(5);
     }
 
-    @And("user navigates to page ten in Home Exchange")
-    public void userNavigatesToPageInHomeExchange() throws AWTException {
-        // Main page
-        page.dropdownMenuButton.click();
-        page.homeExchangeOption.click();
-        // 1st page
-        BrowserUtils.waitFor(1);
-        page.houseButton.click();
-        page.nextButton.click();
-        // 2nd page
-        page.selectText.click();
-        page.selectTextButton.sendKeys("Frankfurt, Almanya");
-        BrowserUtils.waitFor(1);
-        Robot robot = new Robot();
-        robot.keyPress(VK_ENTER);
-        BrowserUtils.waitFor(1);
-        page.checkBox.click();
-        page.nextButton.click();
-        // 3rd page
-        BrowserUtils.waitFor(1);
-        page.sizeOfAreaUp.sendKeys("50");
-        page.nextButton.click();
-        // 4th page
-        BrowserUtils.waitFor(1);
-        page.plus.click();
-        page.nextButton.click();
-        //5th page
-        BrowserUtils.waitFor(1);
-        page.tvText.click();
-        page.nextButton.click();
-        //6th page
-        BrowserUtils.waitFor(1);
-        page.homeExplanation.sendKeys("My home, sweet home.");
-        page.nextButton.click();
-        //7th page
-        BrowserUtils.waitFor(1);
-        page.museumText.click();
-        page.nextButton.click();
-        //8th page
-        for(WebElement element:page.yesTexts){
-            element.click();
+    @And("user navigates to page {int} in Home Exchange")
+    public void userNavigatesToPageInHomeExchange(int sayfa) throws AWTException {
+        while(true){
+            if( sayfa < 1 || sayfa > 10 ){
+                throw new IllegalArgumentException("Not valid page number :"+sayfa);
+            }
+            // Main page
+            page.dropdownMenuButton.click();
+            page.homeExchangeOption.click();
+            if (sayfa == 1)
+                break;
+            // 1st page
+            BrowserUtils.waitFor(1);
+            page.houseButton.click();
+            page.nextButton.click();
+            if (sayfa == 2)
+                break;
+            // 2nd page
+            page.selectText.click();
+            page.selectTextButton.sendKeys("Frankfurt, Almanya");
+            BrowserUtils.waitFor(1);
+            Robot robot = new Robot();
+            robot.keyPress(VK_ENTER);
+            BrowserUtils.waitFor(1);
+            page.checkBox.click();
+            page.nextButton.click();
+            if (sayfa == 3)
+                break;
+            // 3rd page
+            BrowserUtils.waitFor(1);
+            page.sizeOfAreaUp.sendKeys("50");
+            page.nextButton.click();
+            if (sayfa == 4)
+                break;
+            // 4th page
+            BrowserUtils.waitFor(1);
+            page.plus.click();
+            page.nextButton.click();
+            if (sayfa == 5)
+                break;
+            //5th page
+            BrowserUtils.waitFor(1);
+            page.tvText.click();
+            page.nextButton.click();
+            if (sayfa == 6)
+                break;
+            //6th page
+            BrowserUtils.waitFor(1);
+            page.homeExplanation.sendKeys("My home, sweet home.");
+            page.nextButton.click();
+            if (sayfa == 7)
+                break;
+            //7th page
+            BrowserUtils.waitFor(1);
+            page.museumText.click();
+            page.nextButton.click();
+            if (sayfa == 8)
+                break;
+            //8th page
+            for(WebElement element:page.yesTexts){
+                element.click();
+                BrowserUtils.waitFor(1/2);
+            }
+            page.nextButton.click();
+            if (sayfa == 9)
+                break;
+            //9th page
+            BrowserUtils.waitFor(1);
+            page.selectDate.click();
+            page.todaysLocator.click();
             BrowserUtils.waitFor(1/2);
+            page.todaysLocator.click();
+            page.nextButton.click();
         }
-        page.nextButton.click();
-        //9th page
-        BrowserUtils.waitFor(1);
-        page.selectDate.click();
-        page.todaysLocator.click();
-        BrowserUtils.waitFor(1/2);
-        page.todaysLocator.click();
-        page.nextButton.click();
     }
 }
