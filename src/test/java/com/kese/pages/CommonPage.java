f4c20d2a1222bb5da6ab39e407c93b5ccabbba58
nsari <nuraysari.pharm@gmail.com>
2022-01-20 21:29:08 -0600

    nuray

--- src/test/java/com/kese/pages/CommonPage.java
+++ src/test/java/com/kese/pages/CommonPage.java
@@ -1,10 +1,20 @@
 package com.kese.pages;
 
+import com.kese.utilities.BrowserUtils;
 import com.kese.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public abstract class CommonPage {
-    public CommonPage(){
-        PageFactory.initElements(Driver.get(),this);
+
+    public CommonPage() {
+
+
     }
 }
+
+
+
+
---
+++ target/test-classes/features/US079roompicture.feature
@@ -0,0 +1,11 @@
+Feature: US_079
+
+  Background:
+    Given the user should be on the homepage
+    When the user clicks Giris Yap button on HomePage
+    Then page url should be "http://test.kese.nl/login"
+    Then user access to home page
+
+
+   Scenario:
+   Given user click to Ilan Ver dropdown select the Oda
