f01f745f3bf3b0f57a9223d78cfdb7ebc367089f
Mehmet Sen <msen2020@hotmail.com>
2022-01-23 23:33:08 +0100

    US 076 US 077 has been created by MSEN.

---
+++ src/test/java/com/kese/stepdefinitions/US076OdaKiralaPageStepD.java
@@ -0,0 +1,151 @@
+package com.kese.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class US076OdaKiralaPageStepD {
+
+    @And("user clicks Giris Yap Button")
+    public void userClicksGirisYapButton() {
+    }
+
+    @And("user inputs {string} to the email input on the login page")
+    public void userInputsToTheEmailInputOnTheLoginPage(String arg0) {
+    }
+
+    @And("user inputs {string} to the password input on the login page")
+    public void userInputsToThePasswordInputOnTheLoginPage(String arg0) {
+    }
+
+    @And("user clicks on the giris yap button one the login page")
+    public void userClicksOnTheGirisYapButtonOneTheLoginPage() {
+    }
+
+    @And("user clicks on Ilan Ver Button on the main page")
+    public void userClicksOnIlanVerButtonOnTheMainPage() {
+    }
+
+    @And("user clicks on Oda Button on the main page")
+    public void userClicksOnOdaButtonOnTheMainPage() {
+    }
+
+    @And("user is on the {string} new_room page")
+    public void userIsOnTheNew_roomPage(String arg0) {
+    }
+
+    @And("user chooses a House Type on the new room step one page")
+    public void userChoosesAHouseTypeOnTheNewRoomStepOnePage() {
+    }
+
+    @And("user selects Odami Kiraya vermek istiyorum from Degisim and Kirala on the new_room step one page")
+    public void userSelectsOdamiKirayaVermekIstiyorumFromDegisimAndKiralaOnTheNew_roomStepOnePage() {
+    }
+
+    @And("user clicks to the ilerle button on the new_room step one page")
+    public void userClicksToTheIlerleButtonOnTheNew_roomStepOnePage() {
+    }
+
+    @And("user chooses an address from the list in new_room step two page")
+    public void userChoosesAnAddressFromTheListInNew_roomStepTwoPage() {
+    }
+
+    @And("user clicks to the checkbox on the new_room step two page")
+    public void userClicksToTheCheckboxOnTheNew_roomStepTwoPage() {
+    }
+
+    @And("user clicks to the ilerle button on the new_room page")
+    public void userClicksToTheIlerleButtonOnTheNew_roomPage() {
+    }
+
+    @And("user inputs into the usage area on the new_room step three page")
+    public void userInputsIntoTheUsageAreaOnTheNew_roomStepThreePage() {
+    }
+
+    @And("user clicks on the increment buttons on the new_room step three page")
+    public void userClicksOnTheIncrementButtonsOnTheNew_roomStepThreePage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step three page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepThreePage() {
+    }
+
+    @And("user selects Yas Araligi on the new_room step four page")
+    public void userSelectsYasAraligiOnTheNew_roomStepFourPage() {
+    }
+
+    @And("user selects Cinsiyet on the new_room step four page")
+    public void userSelectsCinsiyetOnTheNew_roomStepFourPage() {
+    }
+
+    @And("user increases Kisi Sayisi by clicking on the increment button on the new_room step four page")
+    public void userIncreasesKisiSayisiByClickingOnTheIncrementButtonOnTheNew_roomStepFourPage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step four page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepFourPage() {
+    }
+
+    @And("user select WIFI option from Ev Imkanlari on the new_room step five page")
+    public void userSelectWIFIOptionFromEvImkanlariOnTheNew_roomStepFivePage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room five page page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomFivePagePage() {
+    }
+
+    @And("user inputs a description into the description box on the new_room step six page")
+    public void userInputsADescriptionIntoTheDescriptionBoxOnTheNew_roomStepSixPage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step six page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepSixPage() {
+    }
+
+    @And("user clicks on the Helal Restaurant option from Aktiviteler on the new_room step seven page")
+    public void userClicksOnTheHelalRestaurantOptionFromAktivitelerOnTheNew_roomStepSevenPage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step seven page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepSevenPage() {
+    }
+
+    @And("user clicks on the all Hayir options on the new_room step eight page")
+    public void userClicksOnTheAllHayirOptionsOnTheNew_roomStepEightPage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step eight page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepEightPage() {
+    }
+
+    @And("user selects the date on the new_room step nine page")
+    public void userSelectsTheDateOnTheNew_roomStepNinePage() {
+    }
+
+    @And("user clicks on the ilerle button on the new_room step nine page")
+    public void userClicksOnTheIlerleButtonOnTheNew_roomStepNinePage() {
+    }
+
+    @Then("user verifies that there is a {string} Page Step Number visible on the new room_step ten page")
+    public void userVerifiesThatThereIsAPageStepNumberVisibleOnTheNewRoom_stepTenPage(String arg0) {
+    }
+
+    @Then("user verifies that the title {string} should be visible on the new_room step ten page")
+    public void userVerifiesThatTheTitleShouldBeVisibleOnTheNew_roomStepTenPage(String arg0) {
+    }
+
+    @Then("user verifies that the description of Resim ve Fotograflar should be visible on the new room_step ten page")
+    public void userVerifiesThatTheDescriptionOfResimVeFotograflarShouldBeVisibleOnTheNewRoom_stepTenPage() {
+    }
+
+    @Then("user verifies that Resimleri Secin button is active and functional on the new room_step ten page")
+    public void userVerifiesThatResimleriSecinButtonIsActiveAndFunctionalOnTheNewRoom_stepTenPage() {
+    }
+
+    @Then("user verifies that Tum Resimleri Kaldir button is active and functional on the new room_step ten page")
+    public void userVerifiesThatTumResimleriKaldirButtonIsActiveAndFunctionalOnTheNewRoom_stepTenPage() {
+    }
+
+    @Then("user verifies that the description of the maximum loading pictures should be visible on the new room_step ten page")
+    public void userVerifiesThatTheDescriptionOfTheMaximumLoadingPicturesShouldBeVisibleOnTheNewRoom_stepTenPage() {
+    }
+}
