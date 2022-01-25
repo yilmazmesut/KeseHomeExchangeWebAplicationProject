1a08e77ac32423b80021172f76741e30e8db8963
Rej Arbabov <r.arbabov@gmail.com>
2022-01-25 08:24:37 -0700

    Sprint 2 islemleri yapildi 25 Jan, 8:25

---
+++ src/test/java/com/kese/stepdefinitions/US046HomeExchangePage8StepD.java
@@ -0,0 +1,39 @@
+package com.kese.stepdefinitions;
+
+import com.kese.pages.EvDegisimiPage;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class US046HomeExchangePage8StepD {
+
+    EvDegisimiPage page = new EvDegisimiPage();
+
+    @Then("user asserts ilerle button is visible")
+    public void userAssertsIlerleButtonIsVisible() {
+        Assert.assertTrue(page.ilerleButton.isDisplayed());
+    }
+
+    @Then("user asserts ilerle button should not be clickable as a default")
+    public void userAssertsIlerleButtonShouldNotBeClickableAsADefault() {
+        Assert.assertFalse(page.ilerleButton.isEnabled());
+
+    }
+
+    @Then("user asserts ilerle button should not be clickable if required informations are not filled")
+    public void userAssertsIlerleButtonShouldNotBeClickableIfRequiredInformationsAreNotFilled() {
+        Assert.assertFalse(page.ilerleButton.isEnabled());
+
+    }
+
+    @Then("user asserts ilerle button should be clickable if required informations are filled")
+    public void userAssertsIlerleButtonShouldBeClickableIfRequiredInformationsAreFilled() {
+        page.page8CocuklarinizgetirebilirsinizEVET.click();
+        page.page8EvcilhayvanlarigetirebilirsinizEVET.click();
+        page.page8EvdeSigaraIcmeyeIzinVerilirEVET.click();
+        page.page8ArabaDegisimiOlabilirEVET.click();
+        page.page8BakilmasiGerekEvcilHayvanEVET.click();
+        page.page8BakilmasiGerekenBitkiEVET.click();
+
+        Assert.assertTrue(page.ilerleButton.isEnabled());
+    }
+}
