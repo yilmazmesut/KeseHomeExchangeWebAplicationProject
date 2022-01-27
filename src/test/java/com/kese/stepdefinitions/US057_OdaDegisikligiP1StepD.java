242a9c6986687f95fc13bd1955df207d56676b2f
ufuktakmak <79896070+ufuktakmak@users.noreply.github.com>
2022-01-27 02:05:51 +0100

    updated

---
+++ src/test/java/com/kese/stepdefinitions/US057_OdaDegisikligiP1StepD.java
@@ -0,0 +1,12 @@
+package com.kese.stepdefinitions;
+
+
+
+import io.cucumber.java.en.Then;
+
+public class US057_OdaDegisikligiP1StepD {
+    @Then("User sees {string} warnings")
+    public void userSeesWarnings(String arg0) {
+
+    }
+}
