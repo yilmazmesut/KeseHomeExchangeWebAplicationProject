8d7cb40913f63a5a4582c968d6698373c13ba270
nrslc <cankarnursel@gmail.com>
2022-02-01 10:01:47 +0100

    Nursel- tiny update US080 & US009

--- src/test/java/com/kese/stepdefinitions/Sprint_1_2/US009MainPageStepDefs.java
+++ src/test/java/com/kese/stepdefinitions/Sprint_1_2/US009MainPageStepDefs.java
@@ -12,7 +12,7 @@ public class US009MainPageStepDefs {
     MainPage mainPage = new MainPage();
 
     @Then("Giris Yap button should be visible and clickable on home page")
-    public void giris_Yap_button_should_be_visible_and_clickable_on_home_page() {
+    public void Giris_Yap_button_should_be_visible_and_clickable_on_home_page() {
         BrowserUtils.waitFor(3);
         Assert.assertTrue(mainPage.girisYapButton.isDisplayed());
         Assert.assertTrue(mainPage.girisYapButton.isEnabled());
