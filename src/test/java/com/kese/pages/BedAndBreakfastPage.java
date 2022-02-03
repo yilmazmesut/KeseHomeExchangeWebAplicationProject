632850b53ef56d28fe24e5b3361c2fbedf30e991
aslanmetmet <88263871+aslanmetmet@users.noreply.github.com>
2022-02-02 22:49:52 -0500

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/kese/pages/BedAndBreakfastPage.java
+++ src/test/java/com/kese/pages/BedAndBreakfastPage.java
@@ -1,4 +1,12 @@
 package com.kese.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BedAndBreakfastPage extends CommonPage {
+
+    @FindBy(xpath = "//h2[contains(text(),'Resim ve Fotoğraflar')]")
+    public WebElement ResimVeFotograf;
+
+
 }
