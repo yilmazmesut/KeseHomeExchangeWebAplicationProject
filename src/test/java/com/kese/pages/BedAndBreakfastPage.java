af0510d36ce5490caa6901827d00b53df736b216
Aziz Sahin <60260027+azizshn@users.noreply.github.com>
2022-02-04 21:07:14 +0000

    4FAB_21:00 US083 page 1 locators edited

--- src/test/java/com/kese/pages/BedAndBreakfastPage.java
+++ src/test/java/com/kese/pages/BedAndBreakfastPage.java
@@ -38,10 +38,10 @@ public class BedAndBreakfastPage extends CommonPage {
     @FindBy(xpath = "(//div[@class='border py-3 rounded false'])[1]")
     public WebElement bedAndBreakfastp1Mustakil;
 
-    @FindBy(xpath = "(//div[@class='border py-3 rounded false'])[1]")
+    @FindBy(xpath = "(//div[@class='border py-3 rounded false'])[2]")
     public WebElement bedAndBreakfastp1Apartman;
 
-    @FindBy(xpath = "(//div[@class='border py-3 rounded false'])[1]")
+    @FindBy(xpath = "(//div[@class='border py-3 rounded false'])[3]")
     public WebElement bedAndBreakfastp1Studyo;
 
     @FindBy(xpath = "//*[text()='Odanız hangi ev türünde yer alıyor , belirtiniz.']")
