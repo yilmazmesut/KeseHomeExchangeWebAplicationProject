b0520bed352ab9021bfa52a5e91f563b9afe560e
aslanmetmet <88263871+aslanmetmet@users.noreply.github.com>
2022-02-05 03:04:05 -0500

    3FAB_23:24 Push

---
+++ src/test/java/com/kese/stepdefinitions/Sprint_3/US120_BedAndBreakFastPage10StepD.java
@@ -0,0 +1,40 @@
+package com.kese.stepdefinitions.Sprint_3;
+
+import com.kese.pages.BedAndBreakfastPage;
+import com.kese.pages.EvDegisimiPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US120_BedAndBreakFastPage10StepD {
+
+    BedAndBreakfastPage page1 = new BedAndBreakfastPage();
+
+    @Then("user verifty that there is a {string} Page Step Number visible on the bed and breakfast ten page")
+    public void userVeriftyThatThereIsAPageStepNumberVisibleOnTheBedAndBreakfastTenPage(String arg0) {
+    }
+
+    @Then("user verifty that the title {string} should be visible on the bed and breakfast ten page")
+    public void userVeriftyThatTheTitleShouldBeVisibleOnTheBedAndBreakfastTenPage(String arg0) {
+        
+    }
+
+    @Then("user verifty that the description of Resim ve Fotograflar should be visible on the bed and breakfastp ten page")
+    public void userVeriftyThatTheDescriptionOfResimVeFotograflarShouldBeVisibleOnTheBedAndBreakfastpTenPage() {
+        
+    }
+
+    @Then("user verifty that Tum Resimleri Kaldir button is active and functional on the bed and breakfast  ten page")
+    public void userVeriftyThatTumResimleriKaldirButtonIsActiveAndFunctionalOnTheBedAndBreakfastTenPage() {
+        
+    }
+
+    @Then("user verifty that Resimleri Secin button is active and functional on the bed and breakfast ten page")
+    public void userVeriftyThatResimleriSecinButtonIsActiveAndFunctionalOnTheBedAndBreakfastTenPage() {
+        
+    }
+
+    @Then("user verifty that the description of the maximum loading pictures should be visible on then bed and breakfast ten page")
+    public void userVeriftyThatTheDescriptionOfTheMaximumLoadingPicturesShouldBeVisibleOnThenBedAndBreakfastTenPage() {
+    }
+}
