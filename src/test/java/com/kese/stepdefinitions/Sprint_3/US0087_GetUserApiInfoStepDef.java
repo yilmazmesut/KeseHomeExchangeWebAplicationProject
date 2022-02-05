c853f2d5a963442dcd02c3554f299b6785d23a61
aslanmetmet <88263871+aslanmetmet@users.noreply.github.com>
2022-02-05 00:16:24 -0500

    3FAB_23:24 Push

--- src/test/java/com/kese/stepdefinitions/Sprint_3/US0087_GetUserApiInfoStepDef.java
+++ src/test/java/com/kese/stepdefinitions/Sprint_3/US0087_GetUserApiInfoStepDef.java
@@ -13,45 +13,51 @@ import static org.junit.Assert.assertEquals;
 
 public class US0087_GetUserApiInfoStepDef {
 
-
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxZmQzYzI4NjYwNmNkMzg5MTkzZGE2NyIsImVtYWlsIjoiYXNsYW5AZ21haWwuY29tIiwicm9sIjoia3VsbGFuaWNpIiwia3VsbGFuaWNpX2FkaSI6Im1laG1ldDEyMyIsImR1cnVtIjoxfSwiaWF0IjoxNjQzOTg2MDEwLCJleHAiOjE2NzU1MjIwMTB9.SbaamCjKLtHlUnaWU6AXz7cDntnDjJwRKqO8ilP7opQ";
+    String url = "http://test.kese.nl/api/user/bilgi";
     Response response = null;
     RequestSpecification request = new RequestSpecBuilder()
-                                 .setBaseUri("http://test.kese.nl/api")
-                                 .build();
+            .setBaseUri("http://test.kese.nl/api")
+            .build();
+
 
     @Given("user connect to {string}")
-    public void userConnectToWith(String endPoint) {
+    public void userConnectTo(String endPoint) {
         response = given().
-                queryParam("secret_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxZmQzYzI4NjYwNmNkMzg5MTkzZGE2NyIsImVtYWlsIjoiYXNsYW5AZ21haWwuY29tIiwicm9sIjoia3VsbGFuaWNpIiwia3VsbGFuaWNpX2FkaSI6Im1laG1ldDEyMyIsImR1cnVtIjoxfSwiaWF0IjoxNjQzOTg2MDEwLCJleHAiOjE2NzU1MjIwMTB9.SbaamCjKLtHlUnaWU6AXz7cDntnDjJwRKqO8ilP7opQ").
-                contentType(ContentType.JSON).
+                queryParam("secret_token", token).
+                        contentType(ContentType.JSON).
                 spec(request).
                 get(endPoint);
 
+        System.out.println("response.statusCode() = " + response.statusCode());
+        assertEquals(200, response.getStatusCode());
         response.prettyPrint();
-        System.out.println(response.getStatusCode());
     }
 
     @Then("user verifies that status code is {int}")
     public void userVerifiesThatStatusCodeIs(int arg0) {
-        Json json=new Json();
+        Json json = new Json();
 
         response = given().queryParam("secret_token", token).
                 contentType(ContentType.JSON).
                 spec(request).
                 body(json.toString()).
-                        when().
+                when().
                 patch("/user/account");
         response.prettyPrint();
         System.out.println("response.statusCode() = " + response.statusCode());
-        assertEquals(202,response.statusCode());
-    }
+        assertEquals(202, response.statusCode());
 
+        String token = response.jsonPath().getString("token"); //dinamik token alindi daha sonra istenirse kullanilabilir
+        System.out.println("token = " + token);
     }
+}
 
+/*
     @Then("user verifies that response body has sonuc is {string}")
     public void userVerifiesThatResponseBodyHasSonucIs(String arg0) {
     }
-
+*/
 
 //    @When("I add a book to my reading list")
 //    public void i_add_a_book_to_my_reading_list() {
@@ -66,4 +72,33 @@ public class US0087_GetUserApiInfoStepDef {
 //                .post("/BookStore/v1/Books");
 //
 //    }
-}
+
+
+
+
+
+/*
+@Then("user updates their info and verifies status code")
+public void userUpdatesTheirInfoAndVerifiesStatusCode() {
+        //bu kisim body olarak put yapmak icin
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("email", "azizi.14@gmail.com");
+        jsonObject.put("kullanici_adi", "aziz40");
+
+
+         Map<String, Object> putMap = new HashMap<>();
+         putMap.put("email", "azizi.14@gmail.com");  burayi kullanmadim ama dursun burasi da
+         putMap.put("kullanici_adi", "ahmet");
+
+*//*
+        response = given().queryParam("secret_token", token).
+        contentType(ContentType.JSON).
+        spec(request).
+        body(jsonObject.toString()). //bodyi burda yolluyoruz
+        when().
+        patch("/user/account");
+        response.prettyPrint();
+        System.out.println("response.statusCode() = " + response.statusCode());
+        assertEquals(202,response.statusCode());
+        */
--- src/test/resources/features/Sprint_3_FeatureFiles/Sprint_3_API_Tests/US0087_GetUserApiInfo.feature
+++ src/test/resources/features/Sprint_3_FeatureFiles/Sprint_3_API_Tests/US0087_GetUserApiInfo.feature
@@ -5,6 +5,6 @@ Feature: US_087	As a user, I should be able to see user information when I conne
 
   Scenario: user should be able to see user information
     Given user connect to "/api/user"
-    Then  user verifies that status code is 200
+    #Then  user verifies that status code is 200
    # Then user verifies that response body has sonuc is "true"
 
