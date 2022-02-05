e523793fa3c668ff321df7e510f10113dbf2d162
aslanmetmet <88263871+aslanmetmet@users.noreply.github.com>
2022-02-05 01:06:00 -0500

    3FAB_23:24 Push

--- src/test/java/com/kese/stepdefinitions/Sprint_3/US0087_GetUserApiInfoStepDef.java
+++ src/test/java/com/kese/stepdefinitions/Sprint_3/US0087_GetUserApiInfoStepDef.java
@@ -2,10 +2,12 @@ package com.kese.stepdefinitions.Sprint_3;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.openqa.selenium.json.Json;
 
 import static io.restassured.RestAssured.given;
@@ -23,80 +25,32 @@ public class US0087_GetUserApiInfoStepDef {
     @Given("user connect to {string}")
     public void userConnectTo(String endPoint) {
         response = given().
-                queryParam("secret_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxZmQzYzI4NjYwNmNkMzg5MTkzZGE2NyIsImVtYWlsIjoiYXNsYW5AZ21haWwuY29tIiwicm9sIjoia3VsbGFuaWNpIiwia3VsbGFuaWNpX2FkaSI6Im1laG1ldDEyMyIsImR1cnVtIjoxfSwiaWF0IjoxNjQzOTg2MDEwLCJleHAiOjE2NzU1MjIwMTB9.SbaamCjKLtHlUnaWU6AXz7cDntnDjJwRKqO8ilP7opQ").
-                        contentType(ContentType.JSON).
+                queryParam("id", "61d22c3189708a2d2aeeda1c").
+                contentType(ContentType.JSON).
                 spec(request).
-                get(endPoint);
+                get(endPoint );
 
-        System.out.println("response.statusCode() = " + response.statusCode());
-        response.prettyPrint();
+        System.out.println();
     }
 
     @Then("user verifies that status code is {int}")
-    public void userVerifiesThatStatusCodeIs(int arg0) {
-        Json json = new Json();
+    public void userVerifiesThatStatusCodeIs(int statusCode) {
+      int gelenCevap =response.getStatusCode();
+     // Assert.assertEquals(gelenCevap,statusCode);
 
-        response = given().queryParam("secret_token").
-                contentType(ContentType.JSON).
-                spec(request).
-                body(json.toString()).
-                when().
-                patch("/user/account");
-        response.prettyPrint();
-        System.out.println("response.statusCode() = " + response.statusCode());
-        assertEquals(202, response.statusCode());
 
-        String token = response.jsonPath().getString("token"); //dinamik token alindi daha sonra istenirse kullanilabilir
-        System.out.println("token = " + token);
-    }
-}
+         }
 
-/*
+
     @Then("user verifies that response body has sonuc is {string}")
     public void userVerifiesThatResponseBodyHasSonucIs(String arg0) {
+//String sonucDegeri= response.jsonPath().getString("sonuc");
+    //    System.out.println(sonucDegeri);
+
+
     }
-*/
 
-//    @When("I add a book to my reading list")
-//    public void i_add_a_book_to_my_reading_list() {
-//
-//        RestAssured.baseURI = BASE_URL;
-//        RequestSpecification request = RestAssured.given();
-//        request.header("Authorization", "Bearer " + token)
-//                .header("Content-Type", "application/json");
-//
-//        response = request.body("{ \"userId\": \"" + USER_ID + "\", " +
-//                        "\"collectionOfIsbns\": [ { \"isbn\": \"" + bookId + "\" } ]}")
-//                .post("/BookStore/v1/Books");
-//
-//    }
+}
 
 
 
-
-
-/*
-@Then("user updates their info and verifies status code")
-public void userUpdatesTheirInfoAndVerifiesStatusCode() {
-        //bu kisim body olarak put yapmak icin
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("email", "azizi.14@gmail.com");
-        jsonObject.put("kullanici_adi", "aziz40");
-
-
-         Map<String, Object> putMap = new HashMap<>();
-         putMap.put("email", "azizi.14@gmail.com");  burayi kullanmadim ama dursun burasi da
-         putMap.put("kullanici_adi", "ahmet");
-
-*//*
-        response = given().queryParam("secret_token", token).
-        contentType(ContentType.JSON).
-        spec(request).
-        body(jsonObject.toString()). //bodyi burda yolluyoruz
-        when().
-        patch("/user/account");
-        response.prettyPrint();
-        System.out.println("response.statusCode() = " + response.statusCode());
-        assertEquals(202,response.statusCode());
-        */
