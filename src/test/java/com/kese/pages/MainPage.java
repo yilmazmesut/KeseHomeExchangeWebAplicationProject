bc23f7bb6dbe6d2756e62f717cb95921582b387e
Mehmet Sen <msen2020@hotmail.com>
2022-02-10 23:25:15 +0100

    US 122 bed and Breakfast feature MainPage have been updated by MSEN.

--- src/test/java/com/kese/pages/MainPage.java
+++ src/test/java/com/kese/pages/MainPage.java
@@ -85,7 +85,7 @@ public class MainPage extends CommonPage {
     public WebElement odaKiralaButton;
 
     @FindBy(xpath = "//span[text()='Oda değişimi yapmak istediğiniz yer']")
-    public WebElement odaDegisimiYapmakIstediginizYer;
+    public WebElement odaDegisimiYapmakIstediğinizYer;
 
     public void clickPopulerMenu (String menuTitle){
 
