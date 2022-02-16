76b4ef07727f2536a4a779a5eeb3bd0c9799f58f
nrslself <floraredone@gmail.com>
2022-02-16 23:49:23 +0100

    add US_132

--- src/test/java/com/kese/pages/BirlikteSeyahatPage.java
+++ src/test/java/com/kese/pages/BirlikteSeyahatPage.java
@@ -92,13 +92,17 @@ public class BirlikteSeyahatPage extends CommonPage{
     }
 
     public void setPage4AgeOptions(String age) {
-        String ageLocator = "//span[.='" + age + "']";
-        Driver.get().findElement(By.xpath(ageLocator)).click();
+        if(!age.isEmpty()) {
+            String ageLocator = "//span[.='" + age + "']";
+            Driver.get().findElement(By.xpath(ageLocator)).click();
+        }
     }
 
     public void setPage4GenderOption(String gender) {
-        String genderLocator = "//span[text()='" + gender + "']";
-        Driver.get().findElement(By.xpath(genderLocator)).click();
+        if(!gender.isEmpty()) {
+            String genderLocator = "//span[text()='" + gender + "']";
+            Driver.get().findElement(By.xpath(genderLocator)).click();
+        }
     }
 
     public void setPage5TripDates() {
