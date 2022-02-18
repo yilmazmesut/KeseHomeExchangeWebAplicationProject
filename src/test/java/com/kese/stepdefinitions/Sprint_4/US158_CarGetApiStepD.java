4e0b6982b1432415b23f01592e97432ae88f4d6c
aslanmetmet <88263871+aslanmetmet@users.noreply.github.com>
2022-02-18 03:45:20 -0500

    3FAB_23:27 Push

--- src/test/java/com/kese/stepdefinitions/Sprint_4/US158_CarGetApiStepD.java
+++ src/test/java/com/kese/stepdefinitions/Sprint_4/US158_CarGetApiStepD.java
@@ -3,6 +3,7 @@ package com.kese.stepdefinitions.Sprint_4;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
 
@@ -10,36 +11,41 @@ import static io.restassured.RestAssured.given;
 
 public class US158_CarGetApiStepD {
 
-
+    Response response=null;
     RequestSpecification request = new RequestSpecBuilder()
             .setRelaxedHTTPSValidation()
             .setBaseUri("https://kese.nl/api")
             .build();
 
-    @Given("user connect to {string} about")
+    @Given("user connects to {string} and adds")
     public void userConnectTo(String endPoint) {
 
         given()
                 .when()
                 .then()
-                .statusCode(202);;
+                .statusCode(202);
 
 
     }
 
-    @Then("user I should be able to see the information")
-    public void userIShouldBeAbleToSeeTheInformation(int statusCode) {
 
-        int gelencode=statusCode;
+
+    @Then("user I should be able to see the information {string}")
+    public void userIShouldBeAbleToSeeTheInformation(String sonuc) {
+        String gelenDeger=sonuc;
         given()
                 .relaxedHTTPSValidation()
                 .when()
-                .get("https://kese.nl/api/cars/6203d3276939c771620a1db1")
-                .then();
+                .get("https://kese.nl/car/620f563444c42508ff35fb4c")
+                .then()
+                .statusCode(200)
+                .log().all();
 
-        Assert.assertEquals(gelencode,statusCode);
-        System.out.println("gelenCode: "+ statusCode);
+        Assert.assertEquals(gelenDeger,sonuc);
+        System.out.println("gelenDeger: " + sonuc);
 
     }
-}
 
+
+
+}
--- src/test/resources/features/Sprint_4_FeatureFiles/Sprint_4_API_Tests/US158_Car_Get_Api.feature
+++ src/test/resources/features/Sprint_4_FeatureFiles/Sprint_4_API_Tests/US158_Car_Get_Api.feature
@@ -1,8 +1,6 @@
 Feature: US_158	As a user, when I connect to the "/api/cars/{id}" page with "get" type, I should be able to see the information about the ad.
-#  http://test.kese.nl/api/homes
-#  Kullanıcı giriş yapmış olmalıdır. (token)
-#  "Gönderilecek datalar :  swagger dökümanınde belirtilmiştir"
 
-  Scenario:US158_TC0001 user I should be able to see the information about the ad
-    Given user connect to "/api/cars" about
-    Then user I should be able to see the information
+
+  Scenario: user should be create new car birlikte seyahat advertise
+    Given user connects to "/cars" and adds
+    Then user I should be able to see the information "202"
